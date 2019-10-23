5bf773e940842bf626bc2dd788eea9c51b067507
Qiwen Wang <qwang70@illinois.edu>
2019-10-23 10:19:59 -0700

    extract

---
+++ extract_dataset.py
@@ -0,0 +1,146 @@
+import csv
+import nltk
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+import re
+
+from gensim.models import Word2Vec
+
+from nltk.cluster import KMeansClusterer
+import nltk
+
+
+from sklearn import cluster
+from sklearn import metrics
+
+
+contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not",
+
+                           "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
+
+                           "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",
+
+                           "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would",
+
+                           "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
+
+                           "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam",
+
+                           "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have",
+
+                           "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock",
+
+                           "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have",
+
+                           "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is",
+
+                           "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as",
+
+                           "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would",
+
+                           "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have",
+
+                           "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have",
+
+                           "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are",
+
+                           "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",
+
+                           "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is",
+
+                           "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have",
+
+                           "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have",
+
+                           "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all",
+
+                           "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have",
+
+                           "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+
+                           "you're": "you are", "you've": "you have"}
+
+
+review = []
+y = []
+with open('B007JFMH8M.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        y.append(row[8])
+        review.append(row[9])
+
+
+stop_words = set(stopwords.words('english')) 
+def text_cleaner(text):
+    newString = text.lower()
+    newString = BeautifulSoup(newString, "lxml").text
+    newString = re.sub(r'\([^)]*\)', '', newString)
+    newString = re.sub('"','', newString)
+    newString = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in newString.split(" ")])    
+    newString = re.sub(r"'s\b","",newString)
+    newString = re.sub("[^a-zA-Z]", " ", newString) 
+    tokens = [w for w in newString.split() if not w in stop_words]
+    long_words=[]
+    for i in tokens:
+        if len(i)>=3:                  #removing short word
+            long_words.append(i)   
+    return (" ".join(long_words)).strip()
+
+cleaned_text = []
+for i in range(len(review)):
+    review[i] = text_cleaner(review[i])
+
+def summary_cleaner(text):
+    newString = re.sub('"','', text)
+    newString = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in newString.split(" ")])    
+    newString = re.sub(r"'s\b","",newString)
+    newString = re.sub("[^a-zA-Z]", " ", newString)
+    newString = newString.lower()
+    tokens=newString.split()
+    newString=''
+    for i in tokens:
+        if len(i)>1:                                 
+            newString=newString+i+' '  
+    return newString
+
+#Call the above function
+cleaned_summary = []
+for i in range(len(y)):
+    y[i] = summary_cleaner(y[i])
+
+review = [x.split(" ") for x in review]
+
+# training model
+model = Word2Vec(review, min_count=1)
+# get vector data
+print(dir(model.vocabulary))
+X = model.wv[model.wv.vocab]
+NUM_CLUSTERS=3
+kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance, repeats=25)
+assigned_clusters = kclusterer.cluster(X, assign_clusters=True)
+print (assigned_clusters)
+
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+    print (word + ":" + str(assigned_clusters[i]))
+
+
+
+kmeans = cluster.KMeans(n_clusters=NUM_CLUSTERS)
+kmeans.fit(X)
+
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
+
+print ("Cluster id labels for inputted data")
+print (labels)
+print ("Centroids data")
+print (centroids)
+
+print ("Score (Opposite of the value of X on the K-means objective which is Sum of distances of samples to their closest cluster center):")
+print (kmeans.score(X))
+
+silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
+
+print ("Silhouette_score: ")
+print (silhouette_score)
